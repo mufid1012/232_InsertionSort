810b8a234c984eec9ca874cbf6fa1895a30735de
mufid1012 <mufidghibran1012@gmail.com>
2024-03-14 17:21:39 +0700

    Step 4a-b, simpan arr[j] ke dalam variable arr[j+1] lalu decrement nilai j by 1

--- 1.cpp
+++ 1.cpp
@@ -11,5 +11,13 @@ void InsertionSort(){
          j = i - 1; //step 3
 
          while (j >= 0 && arr[j] > temp) //step 4
+        {
+            arr[j+1] = arr[j]; //step 4a
+            j--; //step 4b
+        }
+
+         
+
+
        
     }
