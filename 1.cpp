c86e5f33a37f1a128faaba8aadd8652eed4577c4
mufid1012 <mufidghibran1012@gmail.com>
2024-03-14 17:26:06 +0700

    Memasukan variabel display

--- 1.cpp
+++ 1.cpp
@@ -41,8 +41,21 @@ void InsertionSort(){
         arr[j + 1] = temp; //step 5
     }
 
+} void display(){
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    for( int j = 0; j < n; j++){
+        cout << arr[j] << endl; //output setiap element array pada garis baru
+    }
+    cout << "jumlah pass = " << n-1  << endl;
+    cout << endl; 
+}
+
+
+
          
 
 
-       
-    }
+    
