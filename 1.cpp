7da77b032b45e82d6bbbfda63d68847b77c4aef5
mufid1012 <mufidghibran1012@gmail.com>
2024-03-14 17:23:41 +0700

    Memasukan variabel input

--- 1.cpp
+++ 1.cpp
@@ -4,6 +4,28 @@ using namespace std;
 int i,j,n,temp;
 int arr[20];
 
+void input(){
+    while (true)
+    {
+        cout <<"masukan banyaknya elemen array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 eleman.\n";
+        }
+    }
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "masukan elemen array" << endl;
+    cout << "====================" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "data ke-" << (i + 1 ) << ": ";
+        cin >> arr[i];
+    }
+}
+
 void InsertionSort(){
  for(i = 1; i <= n-1; i++) { //step 1
         temp = arr[i]; //step 2
